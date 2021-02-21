f9b020f86a4dcb1e40324fd80f144fe23011fe82
GnatykVlad <55984589+GnatykVlad2004@users.noreply.github.com>
2021-02-21 18:41:38 +0200

    Initial commit

---
+++ University_Manager/Models/GroupViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace University_Manager.Models
+{
+    public class GroupViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ University_Manager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(University_Manager.Startup))]
+namespace University_Manager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
